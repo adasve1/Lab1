60d02554aa9ae31b9b88090ccb5e1e3e44e33e1c
adasve1 <73942021+adasve1@users.noreply.github.com>
2020-11-18 11:49:44 +0100

    Added Javadoc

--- B/CarTransport.java
+++ B/CarTransport.java
@@ -22,6 +22,9 @@ public class CarTransport extends MotorVehicle implements Loadable {
         cargo=new ArrayList<>();
     }
 
+    /**
+     * Method which lowers  the ramp. Can only be done if CurrentSpeed is 0.
+     */
     @Override
     public void lower() {
         if (getCurrentSpeed()==0) {
@@ -29,11 +32,19 @@ public class CarTransport extends MotorVehicle implements Loadable {
         }
     }
 
+    /**
+     * Method which is raising the ramp
+     */
     @Override
     public void raise() {
         rampDown=false;
     }
 
+    /**
+     * Method to load a vehicle onto the transport. Can only be done if the distance between the car transport and
+     * the vehicle is less than 10 units adn if there is room on the trailer.
+     * @param m is of the typ MotorVehicle
+     */
     public void load(MotorVehicle m) {
         if (rampDown==true && distanceBetween(m)<=10 && capacityForVehicle(m)==true){
             cargo.add(m);
@@ -43,14 +54,23 @@ public class CarTransport extends MotorVehicle implements Loadable {
         }
     }
 
+    /**
+     * Method to unload from the trailer. Can only be done if the trailer is non-empty and
+     * the ramp is down.
+     */
     public void unload() {
         if (rampDown==true&&cargo.size()>0){
             int index=cargo.size()-1;
             cargo.get(index).setPosition(this.getXPosition(),this.getYPosition());
+            lengthCapacityLeft+=cargo.get(index).getLength();
             cargo.remove(index);
         }
     }
 
+    /**
+     * Method to get the speedfactor of the car transport.
+     * @return 1 % of the total enginepower times a specific weightfactor.
+     */
     @Override
     public double speedFactor(){
         return getEnginePower()*0.01*weightFactor;
@@ -64,18 +84,35 @@ public class CarTransport extends MotorVehicle implements Loadable {
         }
     }
 
+    /**
+     * Method to get the status of the ramp
+     * @return boolean true if ramp is down, false if ramp is up.
+     */
     public boolean getRampStatus(){
         return rampDown;
     }
 
+    /**
+     * Method to get the capacity left on the transport.
+     * @return LengthCapacityLeft
+     */
     public double getLengthCapacityLeft(){
         return lengthCapacityLeft;
     }
 
+    /**
+     * Method to get the list of the cars on the transport.
+     * @return cargo, which is an ArrayList.
+     */
     public ArrayList<MotorVehicle> getCargo(){
         return cargo;
     }
 
+    /**
+     * Method which computes the distance between a vehicle and a car transporter.
+     * @param m MotorVehicle
+     * @return a double with the distance apart.
+     */
     public double distanceBetween(MotorVehicle m){
         double xPosVehicle1=this.getXPosition();
         double xPosVehicle2=m.getXPosition();
@@ -87,10 +124,19 @@ public class CarTransport extends MotorVehicle implements Loadable {
         return distance;
     }
 
+    /**
+     * A method which checks if there is capacity for another car, measured in metres.
+     * @param m is the MotorVehicle we want to check if it fits
+     * @return true if it fits, false if there is no room.
+     */
     public boolean capacityForVehicle(MotorVehicle m){
         return (lengthCapacityLeft>=m.getLength()&&widthCapacity>=m.getWidth());
     }
 
+    /**
+     * Method to use gas on the transporter.
+     * @param amount amount of gas.
+     */
     @Override
     public void gas(double amount){
         if (rampDown==false){
@@ -98,13 +144,14 @@ public class CarTransport extends MotorVehicle implements Loadable {
         }
     }
 
+    /**
+     * Method to start the engine f the Car transport.
+     */
     @Override
     public void startEngine(){
         if (rampDown==false){
             super.startEngine();
         }
     }
-
-
-    //TODO
+    
 }
